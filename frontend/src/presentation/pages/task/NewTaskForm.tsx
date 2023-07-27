f633055fd45d35aec8c6652e80f253a91eeed430
Hasyim-Kai <muhammad.hasyim.c.a@gmail.com>
2023-07-27 21:44:00 +0700

    apply hook in add task

--- frontend/src/presentation/pages/task/NewTaskForm.tsx
+++ frontend/src/presentation/pages/task/NewTaskForm.tsx
@@ -1,31 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import DefaultLayout from "../../layout/Default";
 import { TextField } from "@fluentui/react";
-import { Task } from "../../../infrastructure/task";
+import { Task, fetchPostTask } from "../../../infrastructure/task";
+import useDataMutator from "../../hooks/useMutate";
+import Loading from "../../component/global/Loading";
+import ErrorBanner from "../../component/global/ErrorBanner";
+import { useNavigate } from "react-router-dom";
 
 export default function NewTaskForm() {
+    const navigate = useNavigate()
     const [input, setInput] = useState<Task>({
         title: '',
-        date: '',
         desc: ''
     })
     function handleInput(e: any) {
-        setInput({
-            ...input,
-            [e.target.name]: e.target.value
-        })
+        setInput({ ...input, [e.target.name]: e.target.value })
     }
+    const { data, loading, error, mutateData } = useDataMutator();
     function handleSubmit(e: any) {
         e.preventDefault()
-        console.log(input)
+        mutateData(fetchPostTask, input);
     }
+    useEffect(() => {
+        if (data?.status) { navigate(-1) }
+    }, [data])
 
     return <DefaultLayout>
-        <form className="mx-auto w-8/12" onSubmit={handleSubmit}>
-            <TextField name='title' type="text" label="Title" onChange={handleInput} required />
-            <TextField name='date' type="date" label="Date" onChange={handleInput} />
-            <TextField name='desc' label="Description" onChange={handleInput} multiline resizable={false} rows={10} />
-            <button className="mt-5">Add</button>
-        </form>
+        {loading ? <Loading />
+            : error ? <ErrorBanner />
+                : <form className="mx-auto w-8/12" onSubmit={handleSubmit}>
+                    <TextField name='title' type="text" label="Title" onChange={handleInput} required />
+                    <TextField name='desc' label="Description" onChange={handleInput} multiline resizable={false} rows={10} />
+                    <button className="mt-5 text-xl">+ Add</button>
+                </form>}
     </DefaultLayout>
 }
