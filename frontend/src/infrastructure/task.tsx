0c921bda3eeecc600d4b7bffd3968f923b2fbb84
Hasyim-Kai <muhammad.hasyim.c.a@gmail.com>
2023-07-27 12:59:36 +0700

    prepare api infra

--- frontend/src/infrastructure/task.tsx
+++ frontend/src/infrastructure/task.tsx
@@ -12,17 +12,62 @@ export const dummyTask = [
     {
         title: 'coba 2',
         desc: 'lorem ipsum sdfa  sdfa sadfasdfa sdfasdf asdfgasrgw ef'
-    },
-    {
-        title: 'coba 3',
-        desc: 'aaaaaaaaaaaaaa'
-    },
-    {
-        title: 'coba 4',
-        desc: 'aaaaaaaaaaaaaa'
-    },
-    {
-        title: 'coba 5',
-        desc: 'aaaaaaaaaaaaaa'
-    },
+    }
 ]
+
+const url = `localhost:3000/api/task/`
+
+export const fetchTask = async () => {
+    try {
+        const res = await fetch(url)
+        return await res.json()
+    } catch (error) {
+        return error
+    }
+}
+
+export const fetchPostTask = async (input: Task) => {
+    try {
+        const res = await fetch(url, {
+            method: 'POST',
+            body: JSON.stringify(input),
+            cache: 'default',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        })
+        return await res.json()
+    } catch (error) {
+        return error
+    }
+}
+
+export const fetchUpdateTask = async (input: Task) => {
+    try {
+        const res = await fetch(url, {
+            method: 'UPDATE',
+            body: JSON.stringify(input),
+            cache: 'default',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        })
+        return await res.json()
+    } catch (error) {
+        return error
+    }
+}
+
+export const fetchDelTask = async (id: number | string) => {
+    try {
+        const res = await fetch(url + id, {
+            method: 'DELETE',
+            cache: 'default',
+        })
+        return await res.json()
+    } catch (error) {
+        return error
+    }
+}
