82f290c2f0107bea86b638fe4b0e7ae105d14080
Hasyim-Kai <muhammad.hasyim.c.a@gmail.com>
2023-07-27 21:43:25 +0700

    refactor primary btn

--- frontend/src/presentation/component/global/PrimaryBtn.tsx
+++ frontend/src/presentation/component/global/PrimaryBtn.tsx
@@ -1,12 +1,12 @@
 interface Props {
     txt?: string,
     child?: JSX.Element | JSX.Element[] | null,
-    fn: () => any,
+    fn: any,
     style?: string
 }
 
 export default function PrimaryBtn({ txt, child, fn, style }: Props) {
-    return <button className={`flex gap-5 items-center ${style}`} onClick={fn}>
+    return <button className={`flex gap-5 items-center ${style}`} onClick={() => fn()}>
         {child}
         {txt}
     </button>
