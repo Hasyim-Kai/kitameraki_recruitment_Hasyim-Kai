c43195aafd3ed4ed939b500c03a6538da961ba28
Hasyim-Kai <muhammad.hasyim.c.a@gmail.com>
2023-07-27 11:20:43 +0700

    add navbar to def layout

--- frontend/src/presentation/layout/Default.tsx
+++ frontend/src/presentation/layout/Default.tsx
@@ -1,9 +1,12 @@
+import Navbar from "../component/global/Navbar"
+
 interface Props {
     children: JSX.Element | JSX.Element[] | null
 }
 
 export default function DefaultLayout({ children }: Props) {
     return <main className="min-h-screen">
+        <Navbar />
         {children}
     </main>
 }
