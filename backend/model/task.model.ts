c0ef80dd447c1c5d103a0b5120570d5558113e46
Hasyim-Kai <muhammad.hasyim.c.a@gmail.com>
2023-07-27 21:45:30 +0700

    BE : refactor Task model

--- backend/model/task.model.ts
+++ backend/model/task.model.ts
@@ -8,52 +8,12 @@ export interface Task {
 export const dummyTask: Task[] = [
     {
         id: 0,
-        title: 'Random Data',
-        desc: 'Dummy description for the object.'
+        title: 'Pemuda Tersesat',
+        desc: 'Zuko Ui 3 periode aowkawokwaokwok'
     },
     {
         id: 1,
-        title: 'Sample Title',
-        desc: 'This is a random description.'
+        title: 'saia siapa',
+        desc: 'dimana bantengnya ??'
     },
-    {
-        id: 2,
-        title: 'JavaScript Object',
-        desc: 'Lorem ipsum dolor sit amet.'
-    },
-    {
-        id: 3,
-        title: 'JavaScript Object',
-        desc: 'Dummy description for the object.'
-    },
-    {
-        id: 4,
-        title: 'Sample Title',
-        desc: 'Lorem ipsum dolor sit amet.'
-    },
-    {
-        id: 5,
-        title: 'JavaScript Object',
-        desc: 'This is a random description.'
-    },
-    {
-        id: 6,
-        title: 'Sample Title',
-        desc: 'This is a random description.'
-    },
-    {
-        id: 7,
-        title: 'Lorem Ipsum',
-        desc: 'Another random description.'
-    },
-    {
-        id: 8,
-        title: 'Lorem Ipsum',
-        desc: 'Lorem ipsum dolor sit amet.'
-    },
-    {
-        id: 10,
-        title: 'Random Data',
-        desc: 'Lorem ipsum dolor sit amet.'
-    }
 ]
