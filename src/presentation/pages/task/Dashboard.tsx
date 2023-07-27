4f48864b9d77d4a78baf0b2bc2185e607434ae16
Hasyim-Kai <muhammad.hasyim.c.a@gmail.com>
2023-07-27 10:59:42 +0700

    try add dashboard

--- src/presentation/pages/task/Dashboard.tsx
+++ src/presentation/pages/task/Dashboard.tsx
@@ -1,3 +1,39 @@
+import { ITextFieldStyles, TextField } from "@fluentui/react";
+import { useState } from "react";
+import DefaultLayout from "../../component/layout/Default";
+
 export default function Dashboard() {
-    return <div>Dashboard</div>
+    const [input, setInput] = useState({
+        email: ``,
+        desc: ``
+    })
+    function handleInput(e: any) {
+        setInput({
+            ...input,
+            [e.target.name]: e.target.value
+        })
+    }
+    function handleSubmit(e: any) {
+        e.preventDefault()
+        console.log(input)
+    }
+
+    const textFieldStyles: Partial<ITextFieldStyles> = { fieldGroup: { width: 100 } };
+    return <DefaultLayout>
+        <section>
+            <TextField
+                name="email"
+                label="Basic controlled TextField"
+                onChange={handleInput}
+                styles={textFieldStyles}
+            />
+            <TextField
+                name="desc"
+                label="Controlled TextField limiting length of value to 5"
+                onChange={handleInput}
+                styles={textFieldStyles}
+            />
+            <button onClick={handleSubmit}>aaa</button>
+        </section>
+    </DefaultLayout>
 }
