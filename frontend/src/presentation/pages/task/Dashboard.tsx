2adc565a2315f04f3c94bee8831c28ee41cf46ff
Hasyim-Kai <muhammad.hasyim.c.a@gmail.com>
2023-07-27 21:43:50 +0700

    apply hook in list dashboard

--- frontend/src/presentation/pages/task/Dashboard.tsx
+++ frontend/src/presentation/pages/task/Dashboard.tsx
@@ -1,14 +1,24 @@
 import { useState } from "react";
 import DefaultLayout from "../../layout/Default";
-import { Task, dummyTask } from "../../../infrastructure/task";
+import { Task, fetchTask } from "../../../infrastructure/task";
 import TakListItem from "../../component/features/task/ListItem";
+import PrimaryBtn from "../../component/global/PrimaryBtn";
+import useFetch from "../../hooks/useFetch";
+import Loading from "../../component/global/Loading";
+import ErrorBanner from "../../component/global/ErrorBanner";
 
 export default function Dashboard() {
-    const [tasks, setTasks] = useState<Task[]>([])
+    const [page, setPage] = useState<number>(1)
+    const addPage = () => { setPage(page + 1) }
+    const { data, loading, error } = useFetch(true, fetchTask, page);
 
     return <DefaultLayout>
         <section className="grid grid-cols-1 lg:grid-cols-2 gap-5">
-            {dummyTask.map((task: Task, i: number) => <TakListItem key={i} task={task} />)}
+            {loading ? <Loading />
+                : error ? <ErrorBanner />
+                    : data?.length === 0 ? <div>Cannot Found Anything</div>
+                        : data?.map((task: Task, i: number) => <TakListItem key={i} task={task} />)}
         </section>
+        <PrimaryBtn txt="+ More" style="mt-7" fn={addPage} />
     </DefaultLayout>
 }
