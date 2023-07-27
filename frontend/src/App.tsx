8d7ed360fcdd2eea82bb1a5cbc48337265afb9a4
Hasyim-Kai <muhammad.hasyim.c.a@gmail.com>
2023-07-27 11:45:01 +0700

    add new route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NoPage from "./presentation/pages/NoPage";
 import Dashboard from "./presentation/pages/task/Dashboard";
+import Setting from "./presentation/pages/task/Setting";
+import NewForm from "./presentation/pages/task/NewForm";
 
 export default function App() {
   return <BrowserRouter>
     <Routes>
       <Route path="/" element={<Dashboard />} />
+      <Route path="/setting" element={<Setting />} />
+      <Route path="/add-task" element={<NewForm />} />
       <Route path="*" element={<NoPage />} />
     </Routes>
   </BrowserRouter>
