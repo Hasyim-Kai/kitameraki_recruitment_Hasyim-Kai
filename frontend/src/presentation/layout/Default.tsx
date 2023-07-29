5621e304e8e06337fd589cf30138f301aaeef7b9
Hasyim-Kai <muhammad.hasyim.c.a@gmail.com>
2023-07-29 17:24:22 +0700

    Refactor Default layout

--- frontend/src/presentation/layout/Default.tsx
+++ frontend/src/presentation/layout/Default.tsx
@@ -2,12 +2,14 @@ import Navbar from "../component/global/Navbar"
 
 interface Props {
     children: JSX.Element | JSX.Element[] | null
+    isPadded?: boolean
+    addStyle?: string
 }
 
-export default function DefaultLayout({ children }: Props) {
-    return <main className="min-h-screen">
+export default function DefaultLayout({ children, isPadded = true, addStyle }: Props) {
+    return <main className={`min-h-screen`}>
         <Navbar />
-        <div className="p-7">
+        <div className={`${isPadded ? 'p-7' : 'p-0'} ${addStyle}`}>
             {children}
         </div>
     </main>
