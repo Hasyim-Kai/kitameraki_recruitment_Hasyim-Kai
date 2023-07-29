21bd442c8e55549fd53f5d32502fe8c6b0f3d334
Hasyim-Kai <muhammad.hasyim.c.a@gmail.com>
2023-07-29 17:25:05 +0700

    refactor new task form page to sync with setting

--- frontend/src/presentation/pages/task/NewTaskForm.tsx
+++ frontend/src/presentation/pages/task/NewTaskForm.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import DefaultLayout from "../../layout/Default";
 import { TextField } from "@fluentui/react";
-import { Task, fetchPostTask } from "../../../infrastructure/task";
+import { Task, fetchPostTask, formSettingLsKey } from "../../../infrastructure/task";
 import useDataMutator from "../../hooks/useMutate";
 import Loading from "../../component/global/Loading";
 import ErrorBanner from "../../component/global/ErrorBanner";
@@ -9,6 +9,7 @@ import { useNavigate } from "react-router-dom";
 
 export default function NewTaskForm() {
     const navigate = useNavigate()
+    const [formSetting, setFormSetting] = useState<Task>([])
     const [input, setInput] = useState<Task>({
         title: '',
         desc: ''
@@ -22,6 +23,9 @@ export default function NewTaskForm() {
         mutateData(fetchPostTask, input);
     }
     useEffect(() => {
+        const formSettingJson = localStorage.getItem(formSettingLsKey) || '[]'
+        const formSettingParsed = JSON.parse(formSettingJson);
+        setFormSetting(formSettingParsed)
         if (data?.status) { navigate(-1) }
     }, [data])
 
@@ -31,6 +35,7 @@ export default function NewTaskForm() {
                 : <form className="mx-auto w-8/12" onSubmit={handleSubmit}>
                     <TextField name='title' type="text" label="Title" onChange={handleInput} required />
                     <TextField name='desc' label="Description" onChange={handleInput} multiline resizable={false} rows={10} />
+                    {formSetting.map((item: any) => <TextField key={item.id} name={item.txt} type={item.type} label={item.txt} onChange={handleInput} />)}
                     <button className="mt-5 text-xl">+ Add</button>
                 </form>}
     </DefaultLayout>
